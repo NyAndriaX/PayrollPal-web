f15143f16fb36814e54e300e36e5634694308558
TsilavinaDevJS <tsilavinaandriamahafaly01@gmail.com>
2023-11-02 20:18:16 +0300

    features/UV4-CREATE-MODAL-IN-COMPANY-PAGE

--- src/components/admin/companyList/card/component.card.company.jsx
+++ src/components/admin/companyList/card/component.card.company.jsx
@@ -1,26 +1,52 @@
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
+import ComponentModalFormCompany from "./component.modal.form.company";
 
 const ComponentCardCompany = () => {
+	const [isOpenModalFormCompany, setOpenModalFormCompany] =
+		React.useState(false);
+
+	const onSubmit = (data) => {
+		console.log(data);
+	};
+
+	const openModalFormCompany = () => {
+		setOpenModalFormCompany(true);
+	};
+
+	const closeOpenModalFormCompany = () => {
+		setOpenModalFormCompany(false);
+	};
+
 	return (
-		<div className="row">
-			<div className="card" style={{ backgroundColor: "rgb(255 249 240)" }}>
-				<p className="p-h3" style={{ color: "#ff8a00" }}>
-					<FontAwesomeIcon icon={faBell} /> À connaître
-				</p>
-				<p className="p">
-					Boostez votre entreprise avec FreelPay : des talents freelance, des
-					solutions professionnelles
-				</p>
-			</div>
-			<div className="card">
-				<p className="p-h3-secondary">Nombre des entreprise</p>
-				<div className="row-btn-2">
-					<p className="p-indicateur-secondary">0</p>
-					<button className="btn-secondary">Creer un entreprise</button>
+		<>
+			<div className="row">
+				<div className="card" style={{ backgroundColor: "rgb(255 249 240)" }}>
+					<p className="p-h3" style={{ color: "#ff8a00" }}>
+						<FontAwesomeIcon icon={faBell} /> À connaître
+					</p>
+					<p className="p">
+						Boostez votre entreprise avec FreelPay : des talents freelance, des
+						solutions professionnelles
+					</p>
+				</div>
+				<div className="card">
+					<p className="p-h3-secondary">Nombre des entreprise</p>
+					<div className="row-btn-2">
+						<p className="p-indicateur-secondary">0</p>
+						<button className="btn-secondary" onClick={openModalFormCompany}>
+							Creer un entreprise
+						</button>
+					</div>
 				</div>
 			</div>
-		</div>
+			<ComponentModalFormCompany
+				isOpen={isOpenModalFormCompany}
+				onRequestClose={closeOpenModalFormCompany}
+				onSubmit={onSubmit}
+			/>
+		</>
 	);
 };
 
---
+++ src/components/admin/companyList/card/component.modal.form.company.jsx
@@ -0,0 +1,156 @@
+import React from "react";
+import { useForm, Controller } from "react-hook-form";
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
+
+const steps = [
+	{
+		label: "Étape 1",
+		fields: [
+			"raisonSocial",
+			"adresseDeLEntreprise",
+			"numeroDIdentificationFiscal",
+		],
+	},
+	{
+		label: "Information Representant",
+		fields: [
+			"nomDuRepresentant",
+			"prenomDuRepresentant",
+			"emailDuRepresentant",
+			"téléphoneDuRepresentant",
+			"adresseDuRepresentant",
+		],
+	},
+];
+
+const ComponentModalFormCompany = ({ isOpen, onRequestClose, onSubmit }) => {
+	const [currentStep, setCurrentStep] = React.useState(0);
+	const [isLoading, setIsLoading] = React.useState(false);
+	const [errorRequestMessage, setErrorRequestMessage] = React.useState("");
+	const {
+		control,
+		handleSubmit,
+		formState: { errors, isValid },
+		trigger,
+		getValues,
+	} = useForm({ mode: "onChange" });
+
+	const transformData = (data) => {
+		const transformedData = {
+			raisonSocial: data.raisonSocial || "",
+			adresseEntreprise: data.adresseDeLEntreprise || "",
+			numeroIdentificationFiscale: data.numeroDIdentificationFiscal || "",
+			nomRepresentant: data.nomDuRepresentant || "",
+			emailRepresentant: data.emailDuRepresentant || "",
+			prenomRepresentant: data.prenomDuRepresentant || "",
+			telRepresentant: data.téléphoneDuRepresentant || "",
+			adresseRepresentant: data.adresseDuRepresentant || "",
+		};
+
+		return transformedData;
+	};
+
+	const handleContinueClick = async () => {
+		if (currentStep + 1 <= steps.length) {
+			const isValidStep = await trigger(steps[currentStep].fields);
+			if (isValidStep) {
+				setCurrentStep((prevStep) => prevStep + 1);
+			}
+		}
+	};
+
+	const handleBack = () => {
+		if (currentStep > 0) {
+			setCurrentStep(currentStep - 1);
+		}
+	};
+
+	const handleConfirmAccept = (data) => {
+		onSubmit(data);
+		onRequestClose();
+	};
+
+	return (
+		<Modal
+			isOpen={isOpen}
+			onRequestClose={onRequestClose}
+			contentLabel="Confirmation d'acceptation"
+			className="modal">
+			<p className="p-h1">Inscription</p>
+			<p className="p-h3 text-center">Entrez vos informations</p>
+
+			<form onSubmit={handleSubmit(handleConfirmAccept)}>
+				{steps[currentStep].fields.map((fieldName) => (
+					<div className="auth-input-container" key={fieldName}>
+						<p className={`p-label ${errors[fieldName] ? "p-error" : ""}`}>
+							{fieldName.replace(/([A-Z])/g, " $1").trim()}
+						</p>
+						<Controller
+							name={fieldName}
+							control={control}
+							rules={{
+								required: `Ce champ est requis`,
+								...(fieldName === "emailDuRepresentant" && {
+									pattern: {
+										value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+										message: "Ce champ doit être une adresse e-mail valide",
+									},
+								}),
+								...(fieldName.includes("motDePasse") && {
+									minLength: 8,
+									validate: isPasswordValid,
+								}),
+								...(fieldName === "confirmationDeMotDePasse" && {
+									validate: isPasswordMatch,
+								}),
+							}}
+							render={({ field }) => (
+								<>
+									<input
+										type={
+											fieldName === "motDePasse" ||
+											fieldName === "confirmationDeMotDePasse"
+												? "password"
+												: fieldName === "dateDeNaissance"
+												? "date"
+												: "text"
+										}
+										className={`input ${
+											errors[fieldName] ? "input-error" : ""
+										}`}
+										{...field}
+									/>
+									{errors[fieldName] && (
+										<span className="p-error">{errors[fieldName].message}</span>
+									)}
+								</>
+							)}
+						/>
+					</div>
+				))}
+
+				<button
+					type="button"
+					onClick={
+						currentStep === steps.length - 1
+							? handleSubmit(onSubmit)
+							: handleContinueClick
+					}
+					className="btn-secondary">
+					{isLoading ? (
+						<>Changement ...</>
+					) : (
+						<>{currentStep === steps.length - 1 ? "S'inscrire" : "Continuer"}</>
+					)}
+				</button>
+				<button type="button" className="btn-primary" onClick={handleBack}>
+					Retour
+				</button>
+			</form>
+		</Modal>
+	);
+};
+
+export default ComponentModalFormCompany;
